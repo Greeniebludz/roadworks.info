30be73045134e4f442256dddc2c3b86860ec2d22
Greeniebludz <jamesgreen@live.co.uk>
2026-09-21 14:31:10 +0100

    Refactor script.js for clarity and functionality

--- poc/script.js
+++ poc/script.js
@@ -236,7 +236,7 @@ map.on("zoomend", () => {
   const zoom = map.getZoom();
 
   markers.eachLayer(marker => {
-    if (zoom >= 12) {
+    if (zoom >= 10) {
       marker.setIcon(marker.tmIcon);
     } else {
       marker.setIcon(marker.dotIcon);

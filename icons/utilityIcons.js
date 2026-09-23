0594ca0068a604611fe0cd036340c123449bdcea
Streetworks Oracle™ <jamesgreen@live.co.uk>
2026-09-23 18:40:13 +0100

    Add utility icons for gas, water, electric, and telecom

--- icons/utilityIcons.js
+++ icons/utilityIcons.js
@@ -1,1 +1,6 @@
-
+export const UTILITY_ICONS = {
+  gas: `<path d="M3 0 L6 6 L0 6 Z" fill="#212121"/>`,
+  water: `<path d="M3 0 C5 4, 5 8, 3 10 C1 8, 1 4, 3 0 Z" fill="#212121"/>`,
+  electric: `<path d="M2 0 L6 4 L4 4 L8 10 L2 6 L4 6 Z" fill="#212121"/>`,
+  telecom: `<path d="M1 8 C2 4, 6 4, 7 8" stroke="#212121" stroke-width="2" fill="none"/>`
+};
